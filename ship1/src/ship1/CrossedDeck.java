c0bc2a3f29439824d3ce1767b33a510e2059a71e
Anastasia-Galyamina <52350714+Anastasia-Galyamina@users.noreply.github.com>
2020-01-18 02:44:10 +0400

    Update CrossedDeck.java

--- ship1/src/ship1/CrossedDeck.java
+++ ship1/src/ship1/CrossedDeck.java
@@ -20,15 +20,15 @@ public class CrossedDeck implements IDeck{
 		{			
 		case two:
 			for(int i = 0; i < 200; i += 5)
-			g.drawLine(startPosX + i, startPosY + 50, startPosX + i, startPosY + 80);
+				g.drawLine(startPosX + i, startPosY + 50, startPosX + i, startPosY + 80);
 			for(int j = 50; j < 81; j += 5)
-			g.drawLine(startPosX ,startPosY +  j, startPosX + 200, startPosY + j);
+				g.drawLine(startPosX ,startPosY +  j, startPosX + 200, startPosY + j);
 			for(int i = 50; i <  150; i += 5)
 				g.drawLine(startPosX + i,startPosY +  30,startPosX +  i, startPosY + 50);	
 			for(int j = 30; j < 51; j += 5)
 				g.drawLine(startPosX + 50, startPosY + j, startPosX + 150, startPosY + j);
 			break;
-		case three:	
+		case three:
 			for(int i =  30; i <  60; i += 5)
 				g.drawLine(startPosX + i, startPosY + 15, startPosX + i, startPosY + 25);	
 			for(int j = 15; j < 25; j += 5)
